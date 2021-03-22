13e1134126d8880e22dfcb05fba83bd6e37ed8b0
Andrey <andrey.platonov2005@gmail.com>
2021-03-22 16:54:02 +0300

    site registration
    yeah

--- some_exercises/easy_one/main.py
+++ some_exercises/easy_one/main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 import db_session
 from users import User
 from jobs import Jobs
 import datetime
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -10,9 +11,39 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init('B:/programming/yandex_luceym/flask_learning/flask/some_exercises/easy_one/db/base.db')
-    db_sess = db_session.create_session()
-    for user in db_sess.query(User).filter(User.age < 18):
-        print(f"{user} {user.age} years")
+    app.run()
+
+@app.route('/')
+def index():
+    return render_template("base.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Register Form',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Register Form',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            email=form.email.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            age=form.age.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Register form', form=form)
 
 
 if __name__ == '__main__':
